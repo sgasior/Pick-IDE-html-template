2b19044342e85b92307217c808f35fcaf974683e
Szymon Gąsior <szgasior@outlook.com>
2019-03-29 20:37:20 +0100

    Added animation to the sections & change in height propery of main section & added vendors folder to link lib locally (js and css)

--- js/index.js
+++ js/index.js
@@ -46,3 +46,75 @@ function stickNav() {
 
 
 // --- STICKY NAV END---
+
+
+// -- - ANIMATION START-- -
+
+
+$(document).ready(function () {
+
+
+
+    $(".feature-box:nth-child(1)").css("opacity", 0);
+
+    $(".feature-box:nth-child(1)").waypoint(function () {
+        $(".feature-box:nth-child(1)").addClass("animated bounceInDown");
+        $(".feature-box:nth-child(1)").css("opacity", 1);
+    }, {
+        offset: '80%'
+    });
+
+
+
+    $(".feature-box:nth-child(2)").css("opacity", 0);
+    $(".feature-box:nth-child(2)").waypoint(function () {
+        $(".feature-box:nth-child(2)").addClass("animated bounceInDown");
+        $(".feature-box:nth-child(2)").css("opacity", 1);
+    }, {
+        offset: '80%'
+    });
+
+
+
+    $(".feature-box:nth-child(3)").css("opacity", 0);
+    $(".feature-box:nth-child(3)").waypoint(function () {
+        $(".feature-box:nth-child(3)").addClass("animated bounceInDown");
+        $(".feature-box:nth-child(3)").css("opacity", 1);
+    }, {
+        offset: '80%'
+    });
+
+
+
+    $("#about-app h2").css("opacity", 0);
+    $("#about-app h2").waypoint(function () {
+        $("#about-app h2").addClass("animated bounceIn");
+    }, {
+        offset: '80%'
+    });
+
+
+    $(".step:nth-child(1)").css("opacity", 0);
+    $(".step:nth-child(1)").waypoint(function () {
+        $(".step:nth-child(1)").addClass("animated bounceIn");
+    }, {
+        offset: '80%'
+    });
+
+
+    $(".step:nth-child(2)").css("opacity", 0);
+    $(".step:nth-child(2)").waypoint(function () {
+        $(".step:nth-child(2)").addClass("animated bounceIn");
+    }, {
+        offset: '80%'
+    });
+
+
+    $(".step:nth-child(3)").css("opacity", 0);
+    $(".step:nth-child(3)").waypoint(function () {
+        $(".step:nth-child(3)").addClass("animated bounceIn");
+    }, {
+        offset: '80%'
+    });
+
+});

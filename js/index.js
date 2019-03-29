5caa75f874630ab513ad2a4981ff89de4b6c177e
Szymon Gąsior <szgasior@outlook.com>
2019-03-29 10:22:28 +0100

    Added sticky nav & added and styled features section & some edit on hamburger menu to work fine with sticky nav

--- js/index.js
+++ js/index.js
@@ -6,3 +6,43 @@ $(".hamburger").click(function () {
 });
 
 // --- HAMBURGER END---
+
+// --- STICKY NAV START---
+
+var stickyNav = document.getElementById("sticky-nav");
+
+var navbarBigScreen = document.getElementById("navbar-big-screen-links");
+
+var brand = document.getElementById("brand");
+
+var hamburger = $(".hamburger");
+
+// offset position of the navbar
+var stickyOffsetPosstion = stickyNav.offsetTop;
+
+window.onscroll = function () {
+    stickNav()
+};
+
+
+function stickNav() {
+
+    if (window.pageYOffset >= stickyOffsetPosstion) {
+
+        stickyNav.classList.add("sticky");
+        brand.classList.add("brand-fixed");
+        navbarBigScreen.classList.add("navbar-big-screen-links-fixed");
+
+        hamburger.css("display", "none");
+
+    } else {
+        stickyNav.classList.remove("sticky");
+        navbarBigScreen.classList.remove("navbar-big-screen-links-fixed");
+        brand.classList.remove("brand-fixed");
+
+        hamburger.css("display", "inline-block");
+    }
+}
+
+
+// --- STICKY NAV END---
